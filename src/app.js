ea99090ff52532fc4a01b3e3562e787e8cd4adf7
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2020-10-14 10:48:51 +0800

    add hot reload

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import Vue from 'vue';
 const app = new Vue({
   el: '#app',
   data: {
-    hello: "hello aaaa"
+    hello: "hello aaaa zzz aaa ada aaa dsdsadas"
   },
+  template: '<div id="app">{{hello}}</div>'
 });
 
 export { app };
